67d6e2549d026ab146d786775ba29b296fe77a94
mohammad shirazi <mhmmdshirazi@gmail.com>
2018-09-21 14:19:04 +0430

    felan error dare

---
+++ loadIm.py
@@ -0,0 +1,78 @@
+import random
+from os import walk
+from os import path
+from PIL import Image
+from torchvision import datasets, transforms
+import torch
+from base_funcs import *
+import params
+
+normalize = transforms.Normalize(
+   mean=[0.485, 0.456, 0.406],
+   std=[0.229, 0.224, 0.225]
+)
+preprocess = transforms.Compose([
+   transforms.Scale(64),
+   transforms.ToTensor(),
+   normalize
+])
+
+def LoadData(dir, trainPercentage):
+    train = []
+    trainLabels = []
+    validation = []
+    validLabels = []
+    i = 0
+    for (dirpath, dirnames, filenames) in walk(dir):
+        total = len(filenames)
+        random.shuffle(filenames)
+        for filename in filenames:
+            filepath = path.join(dirpath, filename)
+            img = Image.open(filepath)
+            img_tensor = preprocess(img)
+            if(i <= int(trainPercentage * total)):
+                train.append(img_tensor)
+                trainLabels.append(filepath.split('/')[1])
+            else:
+                validation.append(img_tensor)
+                validLabels.append(filepath.split('/')[1])
+            i = i + 1
+    return train, trainLabels, validation, validLabels
+
+
+trainImgs = []
+trainNames = []
+validImgs =[]
+validNames =[]
+(trainImgs,trainNames,validImgs,validNames) = LoadData('Ttest',0.7)
+
+initialize()
+
+####################### train
+attrList = []
+for trainName in trainNames:
+   attr = train_pics_to_attr_dict[trainName]
+   attrList.append(attr)
+target = torch.FloatTensor(attrList)
+print("kheili alaki")
+def load_train_images(image_size=64, batch_size=64, root="./img/"):
+
+    train_set = trainImgs
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=1)
+    #train_loader.dataset.target = torch.FloatTensor([0,1])
+    return train_loader
+
+####################### validation
+attrListValid = []
+for validName in validNames:
+   attr = train_pics_to_attr_dict[validName]
+   attrListValid.append(attr)
+targetValid = torch.FloatTensor(attrListValid)
+print("kheili alaki")
+def load_train_images_validation(image_size=64, batch_size=64, root="./img/"):
+    train_set = validImgs
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=1)
+    #train_loader.dataset.target = torch.FloatTensor([0,1])
+    return train_loader
+
--- main.py
+++ main.py
@@ -1,6 +1,74 @@
-from base_funcs import *
-import params
+from loadIm import *
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
 
-initialize()
-attr = train_pics_to_attr_dict['3a12cc450615346c97403807ff5371d1.jpeg']
-print(attr)
+#print(attr)
+
+train_loader = load_train_images()
+train_loader_valid = load_train_images_validation()
+####### CNN
+
+targetLearnSize = 30
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5 )
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.mp = nn.MaxPool2d(2)
+        self.fc = nn.Linear(3380, targetLearnSize)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.mp(self.conv1(x)))
+        x = F.relu(self.mp(self.conv2(x)))
+        x = x.view(in_size, -1)  # flatten the tensor
+        x = self.fc(x)
+        return x#F.log_softmax(x)
+
+
+model = Net()
+
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+def train(epoch):
+    model.train()
+    for batch_idx,data in enumerate(train_loader):
+        #data, target = Variable(data), Variable(target)
+        data = Variable(data)
+        optimizer.zero_grad()
+        output = model(data)
+        #print(output)
+        criterion = nn.MSELoss()
+        loss = criterion(output, target)
+        loss.backward()
+        #print(model.conv1.bias.grad)
+
+        optimizer.step()
+
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+def test():
+    for batch_idx,data in enumerate(train_loader_valid):
+        data = Variable(data)
+        output = model(data)
+        output = output.detach().numpy()
+
+        print(batch_idx)
+        for op in output:
+           # m = np.array(op).reshape(-1, 1).transpose()
+            print(atr_to_label(op.reshape(-1,1)[:,0]))
+
+for epoch in range(1, 10):
+    train(epoch)
+
+test()
+
+print("alaki")
